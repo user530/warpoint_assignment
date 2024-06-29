0eeb3f2bdb423c29e7c2b48f5af89034bf76d754
user530 <m.timarsuev@gmail.com>
2024-06-29 18:01:29 +0300

    Added views

---
+++ frontend/src/views/TariffCalculator.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+
+</script>
+
+
+<template>
+    <DefaultLayout>
+        <p>CALCULATOR</p>
+    </DefaultLayout>
+</template>
