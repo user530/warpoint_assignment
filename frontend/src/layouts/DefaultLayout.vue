dfca6138739041eee7703ec742df11a16c379d97
user530 <m.timarsuev@gmail.com>
2024-06-29 18:01:17 +0300

    Layout added

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderDefault from '@/components/HeaderDefault.vue';
+import FooterDefault from '@/components/FooterDefault.vue';
+</script>
+
+<template>
+    <HeaderDefault />
+
+    <main>
+        <slot></slot>
+    </main>
+
+    <FooterDefault />
+</template>
+
+<style scoped></style>
