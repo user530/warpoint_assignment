f3e273a8658995dcb4e900cc5d1d3e50229bd805
user530 <m.timarsuev@gmail.com>
2024-06-29 18:01:03 +0300

    Basic Header and Footer components

---
+++ frontend/src/components/FooterDefault.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer>
+        <p>&copy; All rights reserved</p>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: plum;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ frontend/src/components/HeaderDefault.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>Main Heading</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: plum;
+}
+
+h1 {
+    padding: 0;
+    margin: 0;
+    font-size: 42px;
+    text-align: center;
+    text-transform: uppercase;
+}
+</style>
