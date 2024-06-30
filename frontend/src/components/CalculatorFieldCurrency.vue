0fe5f3a5dcbc31ce69ae241d66d23cba2c9e7df5
user530 <m.timarsuev@gmail.com>
2024-06-30 18:26:02 +0300

    Updated currency component to handle v-model

--- frontend/src/components/CalculatorFieldCurrency.vue
+++ frontend/src/components/CalculatorFieldCurrency.vue
@@ -5,28 +5,31 @@ import { Currency } from '../shared/enums';
 
 interface ICurrencySelectProps extends IFormField {
     baseCurrency: Currency;
+    modelValue: keyof typeof Currency | null;
 }
 
 // Component definitions
 const props = defineProps<ICurrencySelectProps>();
-const emit = defineEmits(['currency-selected']);
+const emit = defineEmits(['update:modelValue']);
 
 const currencies = Object.entries(Currency);
-
-// State variable
-const selectedCurrency = ref<string | null>(currencies[0][0] ?? null);
 
 // Computed props
 const labelText = computed(() => props.label || 'Валюта:');
 const fieldId = computed(() => props.fieldId || 'calculator-currency-selection');
 
-const emitSelection = () => emit('currency-selected', selectedCurrency.value);
+const emitSelection = (event: Event) => {
+    const target = event.target as HTMLSelectElement;
+    emit('update:modelValue', target.value ?? null);
+};
+
 </script>
 
 <template>
     <div>
         <label :for="fieldId">{{ labelText }}</label>
-        <select :id="fieldId" v-model="selectedCurrency" @change="emitSelection">
+        <select :id="fieldId" :value="props.modelValue" @change="emitSelection">
+            <option value="" disabled hidden>{{ props.placeholderText ?? 'Выберите валюту' }}</option>
             <option v-for="currency in currencies" :key="currency[0]" :value="currency[0]">{{ currency[1] }}</option>
         </select>
     </div>
