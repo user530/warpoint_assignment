b41b943b34ce49626879122b9b5529d66f7b9227
user530 <m.timarsuev@gmail.com>
2024-06-30 18:35:42 +0300

    Updated Period component to handle v-model

--- frontend/src/components/CalculatorFieldPeriod.vue
+++ frontend/src/components/CalculatorFieldPeriod.vue
@@ -6,23 +6,27 @@ interface IPeriodSelection extends Omit<IPaymentOption, 'basePrice'> { }
 
 interface IPeriodSelectProps extends IFormField {
     periods: IPeriodSelection[];
+    modelValue: number | null;
 }
 
 const props = defineProps<IPeriodSelectProps>();
-const emit = defineEmits(['period-selected']);
-
-const selectedPeriodId = ref<number | null>(props.periods[0].id ?? null);
+const emit = defineEmits(['update:modelValue']);
 
 const labelText = computed(() => props.label || 'Период оплаты');
 const fieldId = computed(() => props.fieldId || 'calculator-period-selection');
 
-const emitSelection = () => emit('period-selected', selectedPeriodId.value);
+const emitSelection = (e: Event) => {
+    const target = e.target as HTMLSelectElement;
+    emit('update:modelValue', target.value ?? null);
+};
+
 </script>
 
 <template>
     <div>
         <label :for="fieldId">{{ labelText }}</label>
-        <select :id="fieldId" v-model="selectedPeriodId" @change="emitSelection">
+        <select :id="fieldId" :value="modelValue" @change="emitSelection">
+            <option value="" disabled hidden>{{ props.placeholderText ?? 'Выберите период' }}</option>
             <option v-for="period in periods" :key="period.id" :value="period.id">{{ period.paymentPeriod }}</option>
         </select>
     </div>
